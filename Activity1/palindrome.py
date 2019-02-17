381bb54539e192dac37ae9b6c13bfda4d2c1fb10
ileon02 <47202652+ileon02@users.noreply.github.com>
2019-02-16 19:12:07 -0800

    I updated palindrome.py

--- Activity1/palindrome.py
+++ Activity1/palindrome.py
@@ -11,7 +11,40 @@
 # Input: "race a car"
 # Output: false
 
+
 class Solution:
         def isPalindrome(self, s: 'str') -> 'bool':
-                  pass
+            x = len(s)
+            s = s.lower()
+            out = False
+            z = 0
 
+            for n in range(x):
+                if s[z] >= 'a' and s[z] <= 'z':
+                    z += 1
+                    continue
+                else:
+                    s1 = s[:z] + s[z+1:]
+                    s = s1
+                    z -= 1
+                z += 1
+
+            x = len(s)
+
+            for i in range(len(s)):
+                if i < x:
+                    if s[i] == s[x-1]:
+                        out = True
+                    else:
+                        out = False
+                    x -= 1
+                else:
+                    break
+
+            return out
+
+
+y = Solution()
+print(y.isPalindrome("A man, a plan, a canal: Panama"))
+print(y.isPalindrome("Anita lava la tina"))
+print(y.isPalindrome("Race a car"))
