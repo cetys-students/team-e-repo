ddff7ac95392a76dabc6596f8f2ef9ab0ea146ee
Julio Alfonso Rodriguez Olea <julio.rodriguez@cetys.edu.mx>
2019-04-10 00:40:21 -0700

    I updated the coding activity #3

---
+++ Coding Activity #3/Activity3.py
@@ -0,0 +1,24 @@
+
+
+def definite_integral(samples, lower_limit, upper_limit, sampling_period):
+    if lower_limit == upper_limit:
+        return 0.0
+    else:
+        area = 0.5 * (samples[lower_limit] + samples[upper_limit])
+        i = lower_limit
+        while i < upper_limit:
+            area = area + samples[i]
+            i += 1
+        return sampling_period * area
+
+
+def sampleIntegral(samples, sampling_period):
+    if len(samples) < 2:
+        return 0.0
+    area = 0.5 * samples[0]
+    i = 1
+    while i < len(samples) - 1:
+        area = area + samples[i]
+        i += 1
+    area = area + 0.5 * samples[i]
+    return area * sampling_period
